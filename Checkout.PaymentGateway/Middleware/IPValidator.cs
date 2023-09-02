d04cfe1088480c3ed3973dfd09e962941015f962
ifeoluwa osungade <ifeoluwaosungade@gmail.com>
2023-09-02 11:28:15 +0100

    Optimize IP Validator

--- Checkout.PaymentGateway/Middleware/IPValidator.cs
+++ Checkout.PaymentGateway/Middleware/IPValidator.cs
@@ -12,17 +12,16 @@ namespace Checkout.PaymentGateway.Middleware
     // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class IPValidator : ActionFilterAttribute
     {
-        private readonly List<string> _merchantIpAddress;
+        private readonly HashSet<IPAddress> _merchantIpAddress;
 
         public IPValidator(List<IpDto> safeList)
         {
-            _merchantIpAddress = safeList.SelectMany(s => s.Ips).ToList(); //Split(";").ToList();
+            _merchantIpAddress = safeList.SelectMany(s => s.Ips.Select(IPAddress.Parse)).ToHashSet(); //Split(";").ToList();
         }
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             var remoteIp = context.HttpContext.Connection.RemoteIpAddress;
-            
             
             Log.Information("Remote IpAddress: {@RemoteIp}", remoteIp);
             
@@ -32,23 +31,13 @@ namespace Checkout.PaymentGateway.Middleware
             {
                 remoteIp = remoteIp.MapToIPv4();
             }
-            
-            foreach (var address in _merchantIpAddress)
-            {
-                if (IPAddress.Parse(address).Equals(remoteIp))
-                {
-                    badIp = false;
-                    break;
-                }
-            }
 
-            if (badIp)
+            if (!_merchantIpAddress.Contains(remoteIp))
             {
                 Log.Warning("Forbidden Request from IP: {RemoteIp}", remoteIp);
                 context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
                 return;
             }
-
             base.OnActionExecuting(context);
         }
     }
